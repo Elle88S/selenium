73351e093d3dfe04846df7ff356620643285e5d2
Elle <elle.sokolova92@gmail.com>
2024-03-15 21:10:17 +0300

    not working

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -1,26 +1,26 @@
 package ru.netology;
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class OrderCardTest {
-    private WebDriver driver;
-    @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-
 
     @Test
-    void validAmountTest () throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void validAmountTest() {
 
-
+        open("http://localhost:9999/");
+        SelenideElement form = $("[form_size_m form_theme_alfa-on-white]");
+        form.$("[data-test-id=name] input").setValue("Слава");
+        form.$("[data-test-id=phone] input").setValue("89999999999");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        $(".alert-success").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
 
     }
